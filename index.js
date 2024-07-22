4ad67cf4c88f899fac4aaf8fc13ac7635b2c4b74
Nji-bran2 <108367035+Nji-bran2@users.noreply.github.com>
2024-07-22 15:05:45 +0100

    added cors

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ require("dotenv").config()
 var express = require('express');
 var app = express();
 const path = require('path');
+const cors = require('cors');
 //Body parser used to parse the request body
 var bodyParser = require('body-parser');
 var knex = require('./db/db');
 var subscribe = require('./subscribe');
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
